7a489a14fb730878a3ebc279c9257a359106a877
DNereaNicolau <dalmanicolau@gmail.com>
2024-09-26 17:08:02 -0300

    get Notification and send to whatsapp
    action is added and reducer is modified to get the notifications. Functionality is also added to send a text message to members.

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,39 +1,71 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchNotifications } from '../redux/actions/Notifications';
+import { getNotifications } from '../redux/actions/Notifications';
+import { getMembers } from '../redux/actions/Member';
 
 function Notifications() {
   const dispatch = useDispatch();
-  const { notifications, loading, error } = useSelector(state => state.notifications);
+  const { notifications, loading } = useSelector(state => state.notifications);
+  const { members } = useSelector(state => state.members);
+
+  console.log(members);
 
   useEffect(() => {
-    dispatch(fetchNotifications());
+    dispatch(getNotifications());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getMembers());
   }, [dispatch]);
 
   if (loading) {
     return <div className="p-6 text-center text-gray-600">Cargando notificaciones...</div>;
   }
 
-  if (error) {
-    return <div className="p-6 text-center text-red-600">Error al cargar notificaciones: {error}</div>;
-  }
+  console.log(notifications);
+
+  const handleSendWhatsApp = (memberId, expirationDate) => {
+    // Busca al miembro por el ID
+    const memberFound = members.find(m => m._id === memberId);
+    if (!memberFound) {
+      console.error("Miembro no encontrado");
+      return;
+    }
+
+    console.log(memberFound);
+
+    const message = `Hola ${memberFound.name}, ¿cómo estás? Nos comunicamos desde Phisycal Fitness para recordarte que tu plan vence el día ${expirationDate}. Te esperamos nuevamente, que tengas un lindo día!!`;
+    const encodedMessage = encodeURIComponent(message);
+    const phoneNumber = memberFound.cellphone;
+    console.log(phoneNumber);
+    const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+    window.open(url, '_blank');
+  };
 
   return (
     <div className="p-6 bg-gray-50 min-h-screen">
       <h1 className="text-4xl font-semibold mb-8 text-gray-800">Notificaciones</h1>
       <div className="p-6 bg-white rounded-lg shadow-md">
-        {notifications.length > 0 ? (
+        {notifications?.length > 0 ? (
           <ul className="space-y-4">
-            {notifications.map((notification) => (
+            {notifications?.map((notification) => (
               <li key={notification._id} className="border-b pb-4 last:border-b-0">
-                <p className="text-lg font-semibold text-gray-800">{notification.message}</p>
+                <p className="text-lg font-semibold text-gray-800">{notification.title}</p>
                 <p className="text-sm text-gray-500">
-                  Fecha de notificación: {new Date(notification.date).toLocaleDateString("es-ES", {
+                  Fecha de notificación: {new Date(notification.createdAt).toLocaleDateString("es-ES", {
                     year: "numeric",
                     month: "long",
                     day: "numeric",
                   })}
                 </p>
+                {/* Botón para enviar WhatsApp */}
+                <button 
+                  className="mt-2 text-white bg-green-600 hover:bg-green-700 rounded px-4 py-2"
+                  onClick={() => handleSendWhatsApp(notification.member, notification.expirationDate)} // Asegúrate de usar el campo correcto para el ID del miembro
+                >
+                  Enviar WhatsApp
+                </button>
               </li>
             ))}
           </ul>
